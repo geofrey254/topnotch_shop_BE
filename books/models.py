e17e49f68ed7fc82e9cd9a0cf26f9a29dea1a317
geofrey <devongeoffreymaina@gmail.com>
2024-03-19 14:37:44 +0300

    Models creation

--- books/models.py
+++ books/models.py
@@ -1,8 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# CATEGORY MODEL
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    @staticmethod
+    def get_all_categories():
+        return Category.objects.all()
+
+    def __str__(self):
+        return self.name
 
 
 class Books(models.Model):
     title = models.CharField(max_length=255)
-    author = models.CharFiels(max_length=255)
+    author = models.CharField(max_length=255)
+    description = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.title
